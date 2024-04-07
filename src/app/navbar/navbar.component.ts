c07fa33ece4a9589380e758b3b6773c49081b5d0
thatgreenmario <oj1696@gmail.com>
2024-04-07 13:15:23 +0530

    updated signup button behaviour

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgbCollapseModule],
+  imports: [NgIf,NgbCollapseModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
