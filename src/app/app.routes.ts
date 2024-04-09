9c73116aedfcf5ca4cafda77da935786080dff4a
thatgreenmario <oj1696@gmail.com>
2024-04-09 16:10:10 +0530

    Added signup-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SigninPageComponent } from './signin-page/signin-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 export const routes: Routes = [
     {
         path: '',
-        title: 'TaskUp',
+        title: 'TaskUp | Boost Productivity',
         component: LandingPageComponent 
     },
     {
         path: 'sign-in',
         title: 'Sign in to TaskUp',
         component: SigninPageComponent
+    },
+    {
+        path: 'sign-up',
+        title: 'Sign up for a free TaskUp account',
+        component: SignupPageComponent
     }
 ];
